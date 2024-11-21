6eb4655b304feafae86117e868d6c4acb14be40b
CharanKamal Singh <charankamal.singh@crowdbotics.com>
2024-11-21 13:00:13 +0530

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,15 +1,14 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
-  name: "demoapp2111-50109",
-  url: "https://demoapp2111-50109.botics.co",
-  api: "https://demoapp2111-50109.botics.co/api/v1",
-  screenOptions: {
-    headerShown: false
-  }
-}
+  "name": "demoapp2111-50109",
+  "url": "https://demoapp2111-50109.botics.co",
+  "api": "https://demoapp2111-50109.botics.co/api/v1",
+  "screenOptions": {
+    "headerShown": false
+  },
+  "initialRoute": "react_native_appointment"
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
